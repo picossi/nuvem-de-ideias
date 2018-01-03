d0bf3bb93938f3d3b497ee7836bfd8e29f8ade9c
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2018-01-03 20:46:40 -0200

    Adiciona verificação do usuário logado para poder votar
    Closes #5

--- src/app/modules/idea/components/idea-card/idea-card.component.ts
+++ src/app/modules/idea/components/idea-card/idea-card.component.ts
@@ -2,33 +2,47 @@ import { Component, Input, trigger, style, state, transition, animate } from '@a
 import { Idea } from '../../../../shared/models/idea.model';
 import { IdeaService } from '../../idea.service';
 import { AppComponent } from '../../../../app.component';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
-    selector: 'idea-card',
-    templateUrl: './idea-card.component.html',
-    styleUrls: ['./idea-card.component.css']
+  selector: 'idea-card',
+  templateUrl: './idea-card.component.html',
+  styleUrls: ['./idea-card.component.css']
 })
 export class IdeaCardComponent {
-    @Input() idea: Idea;
+  @Input() idea: Idea;
 
-    constructor(
-        public ideaService: IdeaService,
-        public appComponent: AppComponent,
-    ) { }
+  constructor(
+    public ideaService: IdeaService,
+    public appComponent: AppComponent,
+    private snackBar: MatSnackBar
+  ) { }
 
-    upVote() {
-        this.ideaService.update(this.idea.Id, 'UpVote', ++this.idea.UpVote);
+  upVote() {
+    if (this.checkVotePermission()) {
+      this.ideaService.update(this.idea.Id, 'UpVote', ++this.idea.UpVote);
     }
+  }
 
-    downVote() {
-        this.ideaService.update(this.idea.Id, 'DownVote', ++this.idea.DownVote);
+  downVote() {
+    if (this.checkVotePermission()) {
+      this.ideaService.update(this.idea.Id, 'DownVote', ++this.idea.DownVote);
     }
+  }
 
-    remove(idea: Idea) {
-        this.ideaService.update(this.idea.Id, 'Removed', true);
-        this.appComponent.showSnackBar('Ideia removida', 'DESFAZER').onAction().subscribe(() => {
-            this.ideaService.update(this.idea.Id, 'Removed', false);
-        });
+  checkVotePermission(): boolean {
+    if (!this.appComponent.currentUser || !this.appComponent.currentUser.Id) {
+      this.snackBar.open('Você deve fazer o login para votar', 'OK', { duration: 3000 });
+      return false;
     }
+    return true;
+  }
+
+  remove(idea: Idea) {
+    this.ideaService.update(this.idea.Id, 'Removed', true);
+    this.appComponent.showSnackBar('Ideia removida', 'DESFAZER').onAction().subscribe(() => {
+      this.ideaService.update(this.idea.Id, 'Removed', false);
+    });
+  }
 
 }
