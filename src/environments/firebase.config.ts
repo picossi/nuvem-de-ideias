a270bdf9a82ba479cae6d1f27626e195d2c5cf9d
mumumilk <samuel.souza.koch@gmail.com>
2018-01-10 22:09:36 -0200

    removendo arquivo de configuração do firebase

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyDHHkAROwzFb1invthNqeNrk8qTcbubios',
-  authDomain: 'idea-app-72c5a.firebaseapp.com',
-  databaseURL: 'https://idea-app-72c5a.firebaseio.com',
-  projectId: 'idea-app-72c5a',
-  storageBucket: 'idea-app-72c5a.appspot.com',
-  messagingSenderId: '135168410969'
-};
